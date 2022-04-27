a07d7a76fafe049a449809772c88b9db08ce102a
skycat0212 <mrmagnet777@gmail.com>
2022-04-27 22:57:56 +0900

    project init

---
+++ SomeOfSwiftUI/SomeOfSwiftUI/SomeOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeOfSwiftUIApp.swift
+//  SomeOfSwiftUI
+//
+//  Created by 김수진 on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct SomeOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
