d6f39d594c912c8b6fd1b4291d3e2192d2452b0b
skycat0212 <mrmagnet777@gmail.com>
2022-05-01 23:07:02 +0900

    Stack의 종류의 따른 내용을 정의하는 StackInfo enum 구현

--- SomeOfSwiftUI/SomeOfSwiftUI/StackComponent/StackComponentInfoView.swift
+++ SomeOfSwiftUI/SomeOfSwiftUI/StackComponent/StackComponentInfoView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct StackComponentInfoView: View {
     let stackExplanation: String = """
     Stack은 자식뷰를 각각의 방향으로 정렬하여 보여주는 뷰입니다.
-    컨테이너 뷰가 가질 수 있는 직접적인 하위 뷰는 10개로 제한되는 SwiftUI의 특성 상 10개까지의 자식 뷰를 가질 수 있습니다.
+    컨테이너 뷰가 가질 수 있는 직접적인 하위 뷰는 10개로 제한되는 SwiftUI의 특성 상 10개까지의 직접적인 자식 뷰를 가질 수 있습니다.
 
     일반적인 Stack은 LazyStack과 달리, 한 번에 렌더링이 됩니다.
     따라서, 자식뷰가 적거나 지연된 렌더링을 원하지 않을 때 사용합니다.
@@ -34,26 +34,22 @@ struct StackComponentInfoView: View {
 
             Section(header: Text("Stack의 종류")) {
                 NavigationLink("HStack") {
-                    StackDetailInfoView(navigationTitle: "HStack",
-                                        explanation: "HStack은 자식뷰를 수평 방향으로  정렬하여 보여주는 컨테이너 뷰입니다.",
-                                        exampleView: AnyView(Text("예시 뷰")),
-                                        caseView: AnyView(Text("활용예시 뷰")))
-
+                    StackDetailInfoView(stackInfo: .HStack)
                 }
                 NavigationLink("VStack") {
-
+                    StackDetailInfoView(stackInfo: .VStack)
                 }
                 NavigationLink("ZStack") {
-
+                    StackDetailInfoView(stackInfo: .ZStack)
                 }
             }
 
             Section(header: Text("LazyStack의 종류")) {
                 NavigationLink("LazyHStack") {
-
+                    StackDetailInfoView(stackInfo: .LazyHStack)
                 }
                 NavigationLink("LazyVStack") {
-
+                    StackDetailInfoView(stackInfo: .LazyVStack)
                 }
             }
 
--- SomeOfSwiftUI/SomeOfSwiftUI/StackComponent/StackDetailInfoView.swift
+++ SomeOfSwiftUI/SomeOfSwiftUI/StackComponent/StackDetailInfoView.swift
@@ -9,34 +9,30 @@ import SwiftUI
 
 /// Stack의 각각의 종류별로 디테일한 정보를 설명하기 위해 사용되는 뷰
 struct StackDetailInfoView: View {
-    let navigationTitle: String
-    let explanation: String
-    /// 해당 스택을 사용하면 어떻게 표현되는지 나타내는 뷰
-    var exampleView: AnyView
-    /// 해당 스택을 사용해서 어떤 것을 표현할 수 있는지 활용 사례를 나타내는 뷰
-    var caseView: AnyView
+    let stackInfo: StackInfo
 
     var body: some View {
         List {
             // MARK: 설명 섹션
             Section {
-                Text(explanation)
+                Text(stackInfo.explanation)
                     .padding([.top, .bottom])
             }
 
             // MARK: 예시 섹션
             Section(header: Text("예시")) {
-                exampleView
+                stackInfo.exampleView
                     .padding([.top, .bottom])
             }
 
             // MARK: 활용 예시 섹션
             Section(header: Text("활용예제")) {
-                caseView
+                stackInfo.caseView
                     .padding([.top, .bottom])
+                    .listRowBackground(Color.white)
             }
         }
-        .navigationTitle(Text(navigationTitle))
+        .navigationTitle(Text(stackInfo.rawValue))
     }
 }
 
@@ -44,10 +40,11 @@ struct StackDetailInfoView_Previews: PreviewProvider {
     static var previews: some View {
         BaseView()
         StackComponentInfoView()
-        StackDetailInfoView(navigationTitle: "HStack",
-                            explanation: "HStack은 자식뷰를 수평 방향으로  정렬하여 보여주는 컨테이너 뷰입니다.",
-                            exampleView: AnyView(Text("예시뷰")),
-                            caseView: AnyView(Text("활용사례 뷰"))
-        )
+        StackDetailInfoView(stackInfo: .VStack)
     }
+}
+
+func a( number: [Int]) {
+    var number = number
+    number.append(1)
 }
